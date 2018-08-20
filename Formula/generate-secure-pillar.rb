f550c528b9b18bd6d85d362a2885ea4847acc1c5
Ed Silva <ed.silva@everbridge.com>
2018-08-20 10:35:20 -0700

    Brew formula update for generate-secure-pillar version v1.0.401

--- Formula/generate-secure-pillar.rb
+++ Formula/generate-secure-pillar.rb
@@ -1,9 +1,9 @@
 class GenerateSecurePillar < Formula
   desc "Create and update encrypted content or decrypt encrypted content in YAML files"
   homepage "https://github.com/Everbridge/generate-secure-pillar"
-  url "https://github.com/Everbridge/generate-secure-pillar/releases/download/v1.0.397/generate-secure-pillar_1.0.397_Darwin_x86_64.tar.gz"
-  version "1.0.397"
-  sha256 "eace3ccd2e469faede9e10a44fb87e7f4da4a949c0c72cced3421c2fe1593614"
+  url "https://github.com/Everbridge/generate-secure-pillar/releases/download/v1.0.401/generate-secure-pillar_1.0.401_Darwin_x86_64.tar.gz"
+  version "1.0.401"
+  sha256 "47ae12b1b7d8d04f653eb0e0f13da41be68e8e6f86392c7958c2c0c84af83a58"
   
   depends_on "gpg1"
 
